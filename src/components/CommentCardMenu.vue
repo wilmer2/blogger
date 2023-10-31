5d7b1069fac14147aa7cea26563628d4490acee6
Wilmer Maldonado <wilmer.payall@gmail.com>
2023-10-30 20:19:50 -0400

    show menu comment card

--- src/components/CommentCardMenu.vue
+++ src/components/CommentCardMenu.vue
@@ -1,25 +1,70 @@
+<script setup lang="ts">
+import { ref, type Ref } from 'vue'
+
+const showMenu: Ref<boolean> = ref(false)
+const responsive = window.matchMedia('(min-width: 768px)')
+
+const handleHideMenu = (): void => {
+  showMenu.value = false
+}
+
+const handleShowMenu = (): void => {
+  showMenu.value = true
+}
+
+responsive.addEventListener('change', handleHideMenu)
+</script>
+
 <template>
   <div class="p-relative">
     <div class="menu-icon-container">
-      <span class="material-symbols-outlined menu-icon"> more_vert </span>
+      <span class="material-symbols-outlined letter-icon" @click="handleShowMenu"> more_vert </span>
+      <div class="menu-options" v-if="showMenu">
+        <ul>
+          <li>
+            <div class="mr-1">
+              <span class="material-symbols-outlined letter-icon"> dangerous </span>
+            </div>
+            <div class="list-text-container">
+              <span> Quitar contenido </span>
+            </div>
+          </li>
+          <li>
+            <div class="mr-1">
+              <span class="material-symbols-outlined letter-icon"> report </span>
+            </div>
+            <div class="list-text-container">
+              <span>Spam</span>
+            </div>
+          </li>
+          <li>
+            <div class="mr-1">
+              <span class="material-symbols-outlined letter-icon"> delete </span>
+            </div>
+            <div class="list-text-container">
+              <span>Borrar</span>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
 
     <div class="option-icon-container">
       <div class="option-icon">
-        <span class="material-symbols-outlined"> dangerous </span>
+        <span class="material-symbols-outlined letter-icon"> dangerous </span>
       </div>
       <div class="option-icon">
-        <span class="material-symbols-outlined"> report </span>
+        <span class="material-symbols-outlined letter-icon"> report </span>
       </div>
       <div class="option-icon">
-        <span class="material-symbols-outlined"> delete </span>
+        <span class="material-symbols-outlined letter-icon"> delete </span>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-.menu-icon {
+.letter-icon {
   color: #444444;
 }
 
@@ -43,6 +88,34 @@
 
 .option-icon:hover {
   background-color: #eceff1;
+}
+
+ul {
+  position: absolute;
+  top: 24px;
+  left: -202px;
+  padding-top: 8px;
+  padding-bottom: 15px;
+  width: 220px;
+  background-color: white;
+  border: 1px solid var(--border-color);
+  box-shadow: var(--card-shadow);
+  z-index: 999;
+}
+
+li {
+  display: flex;
+  align-items: center;
+  padding: 10px 0 10px 16px;
+  color: #37474f;
+  font-size: 16px;
+}
+
+li:hover {
+  background: var(--bg-primary);
+}
+.list-text-container {
+  padding-bottom: 4px;
 }
 
 @media (width >= 768px) {
