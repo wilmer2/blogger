82ea4447df073f9bc256f02f7fbe5f9493fe61f8
Wilmer Maldonado <wilmer.payall@gmail.com>
2023-11-01 23:47:18 -0400

    show menu

--- src/components/PostSelect.vue
+++ src/components/PostSelect.vue
@@ -1,12 +1,35 @@
+<script setup lang="ts">
+import { ref, type Ref } from 'vue'
+
+const showMenu: Ref<boolean> = ref(false)
+
+const handleShowMenu = (): void => {
+  showMenu.value = true
+}
+</script>
+
 <template>
-  <div class="menu-container">
-    <div class="menu-icon-container eye">
-      <span class="material-symbols-outlined"> visibility </span>
-      <span class="letter-color">Vista previa</span>
+  <div class="p-relative">
+    <div class="menu-container" @click="handleShowMenu">
+      <div class="menu-icon-container eye">
+        <span class="material-symbols-outlined"> visibility </span>
+        <span class="letter-color">Vista previa</span>
+      </div>
+      <div class="menu-icon-container arrow">
+        <span class="material-symbols-outlined"> arrow_drop_down </span>
+      </div>
     </div>
-    <div class="menu-icon-container arrow">
-      <span class="material-symbols-outlined"> arrow_drop_down </span>
-    </div>
+
+    <ul v-if="showMenu">
+      <li>
+        <span class="material-symbols-outlined"> visibility </span>
+        <span class="list-text">Obtener vista previa de la entrada</span>
+      </li>
+      <li>
+        <span class="material-symbols-outlined"> save </span>
+        <span class="list-text">Guardar</span>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -40,7 +63,32 @@
   font-weight: 500;
 }
 
+ul {
+  position: absolute;
+  left: -174px;
+  width: 300px;
+  border: 1px solid var(--border-color);
+  background-color: white;
+  box-shadow: var(--card-shadow);
+  z-index: 999;
+}
+
+li {
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  color: #37474f;
+  font-size: 14px;
+}
+
+.list-text {
+  margin-left: 8px;
+}
+
 @media (width > 840px) {
+  ul {
+    left: -33px;
+  }
   .letter-color {
     display: inline-block;
     margin-left: 5px;
